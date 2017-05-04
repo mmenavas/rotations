6c3a4dd2c8ad35c26e969a07a93e467a4db6352e
Maximo Mena <maximo.mena@gmail.com>
2017-05-03 19:52:55 -0700

    Add functionality to add people.

--- js/main.js
+++ js/main.js
@@ -3,19 +3,26 @@ import { Person } from './Person';
 import { OneOnOne } from './OneOnOne';
 
 let meetup = new OneOnOne();
-let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
-document.getElementById('action-start').addEventListener('click', function() {
-    let total = document.getElementById('count').value;
-    meetup.reset();
-    for (let i = 0; i < total; i++) {
-        meetup.addPerson(new Person(alphabet[i]));
+document.getElementById('action--add-person').addEventListener('click', function() {
+    let name = document.getElementById('new-user').value;
+    if (name !== '') {
+        meetup.addPerson(new Person(name));
+        document.getElementById('new-user').value = '';
+        document.getElementById('output').innerHTML = meetup.getHtmlList();
     }
+    else {
+        document.getElementById('new-user').setAttribute('class', 'validation-error');
+    }
+})
+
+document.getElementById('action--reset').addEventListener('click', function() {
+    meetup.reset();
     document.getElementById('output').innerHTML = meetup.getHtmlList();
     document.getElementById('info-rotations').innerText = meetup.getRotationsCount();
 });
 
-document.getElementById('action-rotate').addEventListener('click', function() {
+document.getElementById('action--rotate').addEventListener('click', function() {
     meetup.rotate();
     document.getElementById('output').innerHTML = meetup.getHtmlList();
     document.getElementById('info-rotations').innerText = meetup.getRotationsCount();
